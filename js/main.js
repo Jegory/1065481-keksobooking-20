0d76e955491319d6f15f0bb10bcccab20438caae
Egor Nazarovich <elf-jegory@yandex.ru>
2020-06-10 12:16:14 +0500

    Модуль-3

--- js/main.js
+++ js/main.js
@@ -1,95 +1,113 @@
-"use strict";
+'use strict';
 
-var ALL = 8;
-var PIN_X = 40;
-var PIN_Y = 80;
-var ADS = [];
+var TOTAL = 8;
 
-var TIMES = ["12.00", "13.00", "14.00"];
+var TIMES = ['12.00', '13.00', '14.00'];
 
-var TYPES = ["palace", "flat", "house", "bungalo"];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 
 var FEATURES = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioners",
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioners',
 ];
 
 var TITLES = [
-  "Топ недорогих отелей",
-  "Топ популярных отелей",
-  "В центре Токио",
-  "На окраине Токио",
-  "Хостел для студентов",
-  "Для молодоженов",
-  "Для семьи с детьми",
-  "VIP-Бунгало",
-  "Коттедж для компании",
-  "Можно с животными",
+  'Топ недорогих отелей',
+  'Топ популярных отелей',
+  'В центре Токио',
+  'На окраине Токио',
+  'Хостел для студентов',
+  'Для молодоженов',
+  'Для семьи с детьми',
+  'VIP-Бунгало',
+  'Коттедж для компании',
+  'Можно с животными',
 ];
 
 var DESCRIPTIONS = [
-  "Идеальный вариант в самом сердце Токио",
-  "Тихие и комфортные аппартаменты в спальном районе Токио, пару остановок и вы в центре",
-  "Светлая квартира с отличным видом на парк",
-  "Бюджетный вариант для студентов с множеством развлечений для молодежи",
-  "Уютные аппартаменты с интернетом и всеми удобствами",
-  "Район с развитой инфраструктурой, парками и развлечениями, как для взрослых так и для детей",
-  "VIP-resort на берегу Токийского залива с видовым бассейном и сауной",
-  "Афрейм с панорамными окнами, большой терассой и зоной барбекю",
+  'Идеальный вариант в самом сердце Токио',
+  'Тихие и комфортные аппартаменты в спальном районе Токио, пару остановок и вы в центре',
+  'Светлая квартира с отличным видом на парк',
+  'Бюджетный вариант для студентов с множеством развлечений для молодежи',
+  'Уютные аппартаменты с интернетом и всеми удобствами',
+  'Район с развитой инфраструктурой, парками и развлечениями, как для взрослых так и для детей',
+  'VIP-resort на берегу Токийского залива с видовым бассейном и сауной',
+  'Афрейм с панорамными окнами, большой терассой и зоной барбекю',
 ];
 
 var PHOTOS = [
-  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel3.jpg",
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
+var map = document.querySelector('.map');
+var pin = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+var filters = document.querySelector('.map__filters')
+var minY = map.offsetHeight - filters.offsetHeight;
+var title = document.querySelector('.map__title');
+var maxY = map.offsetHeight - title.offsetHeight;
+
 // random из числа
-var getRandomNumber = function (min, max) {
+var getRandomNumber = function (max) {
   return Math.floor(Math.random() * (max - min + 1));
 };
 
-// random из массива
-var getRundomArray = function (array) {
-  var newArray = Math.floor(Math.random() * array.length);
-  return array[newArray];
-};
+var getRental = function (count) {
+  var appartments = [];
 
-var rentalAd = function () {
-  var byX = getRandomNumber(300, 900);
-  var byY = getRandomNumber(130, 630);
+  for (var i = 0; i < count; i++) {
+    appartments[i] = {
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png',
+      },
+      offer: {
+        title: TITLES[getRandomNumber(TITLES.length)],
+        address: byX + ', ' + byY,
+        price: getRandomNumber(10000),
+        type: TYPES[getRandomArray(TYPES.length)],
+        rooms: getRandomNumber(20),
+        guests: getRandomNumber(100),
+        checkin: TIMES[getRandomArray(TIMES.length)],
+        checkout: TIMES[getRandomArray(TIMES.length)],
+        features: FEATURES[getRandomArray(FEATURES.length)],
+        description: DESCRIPTIONS[getRandomArray(DESCRIPTIONS.length)],
+        photos: PHOTOS[getRandomArray(PHOTOS.length)]
+      },
+      location: {
+        x: getRandomNumber(map.offsetWidth);
+        y: getRandomNumber(map.offsetHeight);
+      }
+    };
+  }
 
-  var appartmentInfo = {
-    author: {
-      avatar: "img/avatars/user0" + (i + 1) + ".png",
-    },
-    offer: {
-      title: getRundomArray(TITLES),
-      address: byX + ", " + byY,
-      price: getRandomNumber(1000, 10000),
-      type: getRandomArray(TYPES),
-      rooms: getRandomNumber(1, 20),
-      guests: getRandomNumber(0, 100),
-      checkin: getRandomArray(TIMES),
-      checkout: getRandomArray(TIMES),
-      features: getRandomArray(FEATURES),
-      description: getRandomArray(DESCRIPTIONS),
-      photos: getRandomArray(PHOTOS),
-    },
-    location: {
-      x: byX,
-      y: byY,
-    },
-  };
-  return appartmentInfo;
+  return appartments;
 };
 
 // временно убираем класс у блока map
-var map = document.querySelector(".map");
-map.classList.remove("map--faded");
+var showMap = function () {
+  map.classList.remove('map--faded');
+};
 
-var pin = document.querySelector('.map__pins');
+showMap();
+
+
+// + Вызов функции которая будет отбражатть карту
+// функция которая будет генерировать рандомные данные аппартаментов
+// функция которая будет создавать наполненный DOM-элемент данными
+// функция будет делать рендер аппартаментов
+
+/*
+
+"location": {
+  "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
+  "y": случайное число, координата y метки на карте от 130 до 630.
+}
+
+/*
