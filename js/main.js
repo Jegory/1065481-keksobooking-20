ce99d334cdd6953c52f36c86bb3854e442de408a
Egor Nazarovich <elf-jegory@yandex.ru>
2020-06-17 22:27:59 +0500

    Модуль3-3

--- js/main.js
+++ js/main.js
@@ -45,9 +45,43 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
+var RoomtType = {
+  one: '1',
+  two: '2',
+  three: '3',
+  hundret: '100'
+};
+
+var GuestType = {
+  one: '1',
+  two: '2',
+  three: '3',
+  notForGuest: '100'
+};
+
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var mapEvent = document.querySelector('.map__pin--main');
+var form = document.querySelector('.ad-form');
+var fieldsets = document.querySelectorAll('fieldset');
+var selects = document.querySelectorAll('select');
+var inputs = document.querySelectorAll('input');
+var forActive = false;
+
+// form variables (validation)
+var offerCapacity = form.querySelector('#capacity');
+var offerRoomNumber = form.querySelector('#room_number');
+var offerTitle = form.querySelector('#title');
+var offerPrice = form.querySelectorAll('#price');
+
+// x-y variables
+var title = document.querySelector('.map__title');
+var filters = document.querySelector('.map__filters-container');
+var maxY = title.offsetHeight;
+var minY = filters.offsetHeight;
+// map.offsetHeight - (filters.offsetHeight + title.offsetHeight);
 
 // ФУНКЦИЯ random из числа
 var getRandomNumber = function (max, min) {
@@ -78,7 +112,7 @@ var getRental = function (count) {
       },
       location: {
         x: getRandomNumber(map.offsetWidth, 0),
-        y: getRandomNumber(map.offsetHeight, 0)
+        y: getRandomNumber(maxY, minY)
       }
     };
   }
@@ -110,9 +144,25 @@ var createAppartment = function (data) {
   return fragment;
 };
 
+var createFragmentPopup = function (appartment) {
+  var cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__title').textContent = appartment.offer.title;
+  // cardElement.querySelector('.popup__text--adress').textContent = appartment.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = appartment.offer.price + ' ₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = appartment.offer.type;
+  cardElement.querySelector('.popup__text--capacity').textContent = appartment.offer.rooms + ' комнаты для ' + appartment.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + appartment.offer.checkin + ', выезд до ' + appartment.offer.checkout;
+  cardElement.querySelector('.popup__features').textContent = appartment.offer.description;
+  cardElement.querySelector('.popup__avatar').src = appartment.author.avatar;
+
+  return cardElement;
+}
+
+
 // создали рандомный массив объектов и передали 8 эл
 var appartments = getRental(TOTAL);
-// console.log(appartments);
+console.log(appartments);
 
 // наполняем наше дом дерево данными
 var appartmentsElement = createAppartment(appartments);
@@ -120,4 +170,50 @@ var appartmentsElement = createAppartment(appartments);
 // добавляем аппартаменты на карту
 mapPins.appendChild(appartmentsElement);
 
+
+var fragmentPopup = createFragmentPopup(appartments[0]);
+map.appendChild(fragmentPopup);
+
 showMap();
+
+
+// // Module - 4 (total 4 parts)
+
+// 4.1 page activation FUNCTION
+// var activityPage = function (data) {
+//   forActive = true;
+//   map.classList.remove('map--faded');// active map
+//   form.classList.remove('ad-form--disabled');// active form
+//   createAppartment(data);// show all pins on the page
+//   activityForm();
+// };
+
+// // 4.1 form activation FUNCTION (fieldset), (select), (input)
+// var activityForm = function () {
+//   Array.from(fieldsets).forEach(function (fieldset) {
+//     fieldset.disabled = !forActive; // If page is not active, then fieldset off.
+//   });
+//   Array.from(selects).forEach(function (select) {
+//     select.disabled = !forActive; // select off.
+//   });
+//   Array.from(inputs).forEach(function (input) {
+//     input.disabled = !forActive; // input off.
+//   });
+// };
+
+// // 4.2 page activation EVENT
+// var activePage = function (data) {
+//   mapEvent.addEventListener('keydown', function (evt) {
+//     if (evt.keyCode === 13) {
+//       evt.preventDefault();
+//       activityPage(data);
+//     }
+//   });
+
+//   mapEvent.addEventListener('mousedown', function (evt) {
+//     if (evt.which === 0) {
+//       evt.preventDefault();
+//       activityPage(data);
+//     }
+//   });
+// };
